e0b36387a48030a3561f9df5b93be98a59eb8ce0
Max <a17maxsj@student.his.se>
2020-04-16 15:57:37 +0200

    streamlined var names

--- d3JS/d3.js
+++ d3JS/d3.js
@@ -2,7 +2,7 @@ function d3js() {
 
 
     console.log("d3");
-    console.log(list[0]);
+    console.log(dList[0]);
 
 
     var sampleSVG = d3.select("#svg-container")
@@ -13,55 +13,54 @@ function d3js() {
     var myimage = sampleSVG.append('image')
         .attr('xlink:href', 'img/map.svg')
         .attr('width', 900)
-        .attr('height', 600)
-    var size = 5
+        .attr('height', 600);
 
     var i;
-    for (i = 0; i < list.length; i++) {
-        console.log(i + "medelålder: " + list[i].medel)
+    for (i = 0; i < dList.length; i++) {
+        console.log(i + "medelålder: " + dList[i].dValue)
 
-        if (list[i].lan === "01") {
-            drawCircle(460, 430, 5, "Stockholm: " + list[i].medel);
-        } else if (list[i].lan === "03") {
-            drawCircle(450, 410, 5, "Uppsala: " + list[i].medel);
-        } else if (list[i].lan === "04") {
-            drawCircle(435, 445, 5, "Södermanland: " + list[i].medel);
-        } else if (list[i].lan === "05") {
-            drawCircle(420, 470, 5, "Östergötland: " + list[i].medel);
-        } else if (list[i].lan === "06") {
-            drawCircle(400, 500, 5, "Jönköping: " + list[i].medel);
-        } else if (list[i].lan === "07") {
-            drawCircle(405, 525, 5, "Kronoberg: " + list[i].medel);
-        } else if (list[i].lan === "08") {
-            drawCircle(425, 510, 5, "Kalmar: " + list[i].medel);
-        } else if (list[i].lan === "09") {
-            drawCircle(465, 505, 5, "Gotland: " + list[i].medel);
-        } else if (list[i].lan === "10") {
-            drawCircle(418, 542, 5, "Blekinge: " + list[i].medel);
-        } else if (list[i].lan === "12") {
-            drawCircle(390, 550, 5, "Skåne: " + list[i].medel);
-        } else if (list[i].lan === "13") {
-            drawCircle(380, 525, 5, "Halland: " + list[i].medel);
-        } else if (list[i].lan === "14") {
-            drawCircle(380, 475, 5, "Västra götaland: " + list[i].medel);
-        } else if (list[i].lan === "17") {
-            drawCircle(380, 420, 5, "Värmland: " + list[i].medel);
-        } else if (list[i].lan === "18") {
-            drawCircle(410, 430, 5, "Örebro: " + list[i].medel);
-        } else if (list[i].lan === "19") {
-            drawCircle(430, 420, 5, "Västmanland: " + list[i].medel);
-        } else if (list[i].lan === "20") {
-            drawCircle(405, 380, 5, "Dalarna: " + list[i].medel);
-        } else if (list[i].lan === "21") {
-            drawCircle(435, 370, 5, "Gävleborg: " + list[i].medel);
-        } else if (list[i].lan === "22") {
-            drawCircle(449, 300, 5, "Västernorrland: " + list[i].medel);
-        } else if (list[i].lan === "23") {
-            drawCircle(400, 300, 5, "Jämtland: " + list[i].medel);
-        } else if (list[i].lan === "24") {
-            drawCircle(450, 220, 5, "Västerbotten: " + list[i].medel);
-        } else if (list[i].lan === "25") {
-            drawCircle(480, 120, 5, "Norrbotten: " + list[i].medel);
+        if (dList[i].lan === "01") {
+            drawCircle(460, 430, 5, "Stockholm: " + dList[i].dValue);
+        } else if (dList[i].lan === "03") {
+            drawCircle(450, 410, 5, "Uppsala: " + dList[i].dValue);
+        } else if (dList[i].lan === "04") {
+            drawCircle(435, 445, 5, "Södermanland: " + dList[i].dValue);
+        } else if (dList[i].lan === "05") {
+            drawCircle(420, 470, 5, "Östergötland: " + dList[i].dValue);
+        } else if (dList[i].lan === "06") {
+            drawCircle(400, 500, 5, "Jönköping: " + dList[i].dValue);
+        } else if (dList[i].lan === "07") {
+            drawCircle(405, 525, 5, "Kronoberg: " + dList[i].dValue);
+        } else if (dList[i].lan === "08") {
+            drawCircle(425, 510, 5, "Kalmar: " + dList[i].dValue);
+        } else if (dList[i].lan === "09") {
+            drawCircle(465, 505, 5, "Gotland: " + dList[i].dValue);
+        } else if (dList[i].lan === "10") {
+            drawCircle(418, 542, 5, "Blekinge: " + dList[i].dValue);
+        } else if (dList[i].lan === "12") {
+            drawCircle(390, 550, 5, "Skåne: " + dList[i].dValue);
+        } else if (dList[i].lan === "13") {
+            drawCircle(380, 525, 5, "Halland: " + dList[i].dValue);
+        } else if (dList[i].lan === "14") {
+            drawCircle(380, 475, 5, "Västra götaland: " + dList[i].dValue);
+        } else if (dList[i].lan === "17") {
+            drawCircle(380, 420, 5, "Värmland: " + dList[i].dValue);
+        } else if (dList[i].lan === "18") {
+            drawCircle(410, 430, 5, "Örebro: " + dList[i].dValue);
+        } else if (dList[i].lan === "19") {
+            drawCircle(430, 420, 5, "Västmanland: " + dList[i].dValue);
+        } else if (dList[i].lan === "20") {
+            drawCircle(405, 380, 5, "Dalarna: " + dList[i].dValue);
+        } else if (dList[i].lan === "21") {
+            drawCircle(435, 370, 5, "Gävleborg: " + dList[i].dValue);
+        } else if (dList[i].lan === "22") {
+            drawCircle(449, 300, 5, "Västernorrland: " + dList[i].dValue);
+        } else if (dList[i].lan === "23") {
+            drawCircle(400, 300, 5, "Jämtland: " + dList[i].dValue);
+        } else if (dList[i].lan === "24") {
+            drawCircle(450, 220, 5, "Västerbotten: " + dList[i].dValue);
+        } else if (dList[i].lan === "25") {
+            drawCircle(480, 120, 5, "Norrbotten: " + dList[i].dValue);
         } else {
             console.log("fail");
             break;
@@ -70,10 +69,11 @@ function d3js() {
 
     }
     /* test draw
-    drawCircle(400, 400, 10, list[0]);
-    drawCircle(500, 100, 5 ,list[1]);
-    drawCircle(450, 220, 5, list[2]);
+    drawCircle(400, 400, 10, dList[0]);
+    drawCircle(500, 100, 5 ,dList[1]);
+    drawCircle(450, 220, 5, dList[2]);
     */
+
     function drawCircle(x, y, size, tip) {
 
         console.log('Drawing circle at', x, y, size);
--- script/api.js
+++ script/api.js
@@ -1,102 +1,98 @@
 window.onload = init;
-const list = [];
-
-
+const dList = [];
 
 function init() {
-  console.log("starting");
-  //Json question
-  var jsonObj =
-  {
-    "query": [
-      {
-        "code": "Region",
-        "selection": {
-          "filter": "vs:RegionLän99EjAggr",
-          "values": [
-            "01",
-            "03",
-            "04",
-            "05",
-            "06",
-            "07",
-            "08",
-            "09",
-            "10",
-            "12",
-            "13",
-            "14",
-            "17",
-            "18",
-            "19",
-            "20",
-            "21",
-            "22",
-            "23",
-            "24",
-            "25"
-          ]
+    console.log("starting");
+    //Json question
+    var jsonObj = {
+        "query": [{
+                "code": "Region",
+                "selection": {
+                    "filter": "vs:RegionLän99EjAggr",
+                    "values": [
+                        "01",
+                        "03",
+                        "04",
+                        "05",
+                        "06",
+                        "07",
+                        "08",
+                        "09",
+                        "10",
+                        "12",
+                        "13",
+                        "14",
+                        "17",
+                        "18",
+                        "19",
+                        "20",
+                        "21",
+                        "22",
+                        "23",
+                        "24",
+                        "25"
+                    ]
+                }
+            },
+            {
+                "code": "Kon",
+                "selection": {
+                    "filter": "item",
+                    "values": [
+                        "1+2"
+                    ]
+                }
+            },
+            {
+                "code": "Tid",
+                "selection": {
+                    "filter": "item",
+                    "values": [
+                        "2019"
+                    ]
+                }
+            }
+        ],
+        "response": {
+            "format": "json"
         }
-      },
-      {
-        "code": "Kon",
-        "selection": {
-          "filter": "item",
-          "values": [
-            "1+2"
-          ]
+
+    };
+
+    $.ajax({
+        url: "https://api.scb.se/OV0104/v1/doris/sv/ssd/START/BE/BE0101/BE0101B/BefolkningMedelAlder",
+        type: "POST",
+        data: JSON.stringify(jsonObj),
+        dataType: "json",
+        success: function(obj) {
+
+            $.each(obj.data, function(index, data) {
+                    /*
+                    //Testing, log values in console
+                      console.log(data.key[0]);
+                      console.log(data.key[1]);
+                      console.log(data.key[2]);
+                      console.log(data.values);
+                      */
+
+                    //Testing, display all values
+                    document.getElementById("lanInfo").innerHTML +=
+                        "<p>Län: " + data.key[0] + " År: " + data.key[2] + " Medelålder: " + data.values + "</p>";
+
+
+                    // put all the values in an array
+                    dList.push({ lan: data.key[0], ar: data.key[2], dValue: data.values[0] });
+
+
+                }
+
+            );
+
         }
-      },
-      {
-        "code": "Tid",
-        "selection": {
-          "filter": "item",
-          "values": [
-            "2019"
-          ]
-        }
-      }
-    ],
-    "response": {
-      "format": "json"
-    }
 
-  };
+    });
 
-  $.ajax({
-    url: "https://api.scb.se/OV0104/v1/doris/sv/ssd/START/BE/BE0101/BE0101B/BefolkningMedelAlder",
-    type: "POST",
-    data: JSON.stringify(jsonObj),
-    dataType: "json",
-    success: function (obj) {
-
-      $.each(obj.data, function (index, data) {
-        /*
-        //Testing, log values in console
-          console.log(data.key[0]);
-          console.log(data.key[1]);
-          console.log(data.key[2]);
-          console.log(data.values);
-          */
-
-        //Testing, display all values
-        document.getElementById("lanInfo").innerHTML +=
-          "<p>Län: " + data.key[0] + " År: " + data.key[2] + " Medelålder: " + data.values + "</p>";
-
-
-        // put all the values in an array
-        list.push({ lan: data.key[0], ar: data.key[2], medel: data.values[0] });
-
-
-      }
-
-      );
-
-    }
-
-  });
-
-  console.log(list);
+    console.log(dList);
 
 
 
