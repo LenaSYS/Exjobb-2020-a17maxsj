39ea49d0346c26366cc1124c5a63ad52021e2254
Max <a17maxsj@student.his.se>
2020-04-16 15:51:16 +0200

    Dots are correctomundo

--- d3JS/d3.js
+++ d3JS/d3.js
@@ -1,59 +1,94 @@
-
-    
 function d3js() {
 
-    
-console.log("d3");
-console.log(list[0]);
+
+    console.log("d3");
+    console.log(list[0]);
 
 
-  var sampleSVG = d3.select("#svg-container")
-  .append("svg")
-  .attr("width", 900)
-  .attr("height", 700);
+    var sampleSVG = d3.select("#svg-container")
+        .append("svg")
+        .attr("width", 900)
+        .attr("height", 700);
 
     var myimage = sampleSVG.append('image')
         .attr('xlink:href', 'img/map.svg')
         .attr('width', 900)
         .attr('height', 600)
-var size = 5
+    var size = 5
 
-var i;
-for (i = 0; i < list.length; i++) {
-console.log(i + "medelålder: "  + list[i].medel)
+    var i;
+    for (i = 0; i < list.length; i++) {
+        console.log(i + "medelålder: " + list[i].medel)
 
-if(list[i].lan === "01"){
-    drawCircle(500, 400, 5, "Stockholm: " + list[i].medel);
+        if (list[i].lan === "01") {
+            drawCircle(460, 430, 5, "Stockholm: " + list[i].medel);
+        } else if (list[i].lan === "03") {
+            drawCircle(450, 410, 5, "Uppsala: " + list[i].medel);
+        } else if (list[i].lan === "04") {
+            drawCircle(435, 445, 5, "Södermanland: " + list[i].medel);
+        } else if (list[i].lan === "05") {
+            drawCircle(420, 470, 5, "Östergötland: " + list[i].medel);
+        } else if (list[i].lan === "06") {
+            drawCircle(400, 500, 5, "Jönköping: " + list[i].medel);
+        } else if (list[i].lan === "07") {
+            drawCircle(405, 525, 5, "Kronoberg: " + list[i].medel);
+        } else if (list[i].lan === "08") {
+            drawCircle(425, 510, 5, "Kalmar: " + list[i].medel);
+        } else if (list[i].lan === "09") {
+            drawCircle(465, 505, 5, "Gotland: " + list[i].medel);
+        } else if (list[i].lan === "10") {
+            drawCircle(418, 542, 5, "Blekinge: " + list[i].medel);
+        } else if (list[i].lan === "12") {
+            drawCircle(390, 550, 5, "Skåne: " + list[i].medel);
+        } else if (list[i].lan === "13") {
+            drawCircle(380, 525, 5, "Halland: " + list[i].medel);
+        } else if (list[i].lan === "14") {
+            drawCircle(380, 475, 5, "Västra götaland: " + list[i].medel);
+        } else if (list[i].lan === "17") {
+            drawCircle(380, 420, 5, "Värmland: " + list[i].medel);
+        } else if (list[i].lan === "18") {
+            drawCircle(410, 430, 5, "Örebro: " + list[i].medel);
+        } else if (list[i].lan === "19") {
+            drawCircle(430, 420, 5, "Västmanland: " + list[i].medel);
+        } else if (list[i].lan === "20") {
+            drawCircle(405, 380, 5, "Dalarna: " + list[i].medel);
+        } else if (list[i].lan === "21") {
+            drawCircle(435, 370, 5, "Gävleborg: " + list[i].medel);
+        } else if (list[i].lan === "22") {
+            drawCircle(449, 300, 5, "Västernorrland: " + list[i].medel);
+        } else if (list[i].lan === "23") {
+            drawCircle(400, 300, 5, "Jämtland: " + list[i].medel);
+        } else if (list[i].lan === "24") {
+            drawCircle(450, 220, 5, "Västerbotten: " + list[i].medel);
+        } else if (list[i].lan === "25") {
+            drawCircle(480, 120, 5, "Norrbotten: " + list[i].medel);
+        } else {
+            console.log("fail");
+            break;
+        }
 
-}else if(list[i].lan === "03"){
-    drawCircle(200, i*10+50, 5, list[i].medel);
-}else {
-    console.log("fail");
-    break;
-}
 
-
-}
-/*
-drawCircle(400, 400, 10, list[0]);
-drawCircle(500, 100, 5 ,list[1]);
-drawCircle(450, 220, 5, list[2]);
-*/
-function drawCircle(x, y, size, tip) {
+    }
+    /* test draw
+    drawCircle(400, 400, 10, list[0]);
+    drawCircle(500, 100, 5 ,list[1]);
+    drawCircle(450, 220, 5, list[2]);
+    */
+    function drawCircle(x, y, size, tip) {
 
         console.log('Drawing circle at', x, y, size);
         sampleSVG.append("circle")
-       
+
         .style("stroke", "gray")
-        .style("fill", "blue")
+            .style("fill", "blue")
             .attr("cx", x)
             .attr("cy", y)
             .attr("r", size)
-            .on("mouseover", function(){return tooltip.style("visibility", "visible");})
-	        .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
-            .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
-            
-            var tooltip = d3.select("body")
+            .on("mouseover", function() { return tooltip.style("visibility", "visible"); })
+            .on("mousemove", function() { return tooltip.style("top", (event.pageY - 10) + "px").style("left", (event.pageX + 10) + "px"); })
+            .on("mouseout", function() { return tooltip.style("visibility", "hidden"); });
+
+        var tooltip = d3.select("body")
             .append("div")
             .style("position", "absolute")
             .style("z-index", "10")
@@ -62,8 +97,7 @@ function drawCircle(x, y, size, tip) {
             .style("background-color", "black")
             .text(tip);
     }
-  
+
 
 
 }
-
--- threeJS/three.js
+++ threeJS/three.js
@@ -1,5 +1,5 @@
 function threejs() {
-    console.log("lolman")
+  console.log("lolman")
 
   /*
     var can = document.getElementById('artifactCanvas');
@@ -12,121 +12,121 @@ var ctx = can.getContext('2d');
     img.src = "img/map.svg";
 */
 
-this.renderer = new THREE.WebGLRenderer( { canvas: artifactCanvas, alpha: true } );
+  this.renderer = new THREE.WebGLRenderer({ canvas: artifactCanvas, alpha: true });
 
 
 
-////////////////// CAMERA
-this.camera = new THREE.PerspectiveCamera(
-  35,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  3000
-);
-this.camera.position.set(0, 0, 200);
+  ////////////////// CAMERA
+  this.camera = new THREE.PerspectiveCamera(
+    35,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    3000
+  );
+  this.camera.position.set(0, 0, 200);
 
-////////////////// SCENE
-this.scene = new THREE.Scene();
+  ////////////////// SCENE
+  this.scene = new THREE.Scene();
 
-const scene = this.scene;
-scene.background = new THREE.Color( 0xff0000 );
+  const scene = this.scene;
+  scene.background = new THREE.Color(0xff0000);
 
 
-////////////////// LOADER
-fetch( 'https://openclipart.org/download/219211/options_icon.svg' )
-    .then( response => response.text() )
-    .then( svg => {
+  ////////////////// LOADER
+  fetch('https://openclipart.org/download/219211/options_icon.svg')
+    .then(response => response.text())
+    .then(svg => {
 
-      var node = document.createElementNS( 'http://www.w3.org/2000/svg', 'g' );
+      var node = document.createElementNS('http://www.w3.org/2000/svg', 'g');
       var parser = new DOMParser();
-      var doc = parser.parseFromString( svg, 'image/svg+xml' );
+      var doc = parser.parseFromString(svg, 'image/svg+xml');
 
-      node.appendChild( doc.documentElement );
+      node.appendChild(doc.documentElement);
 
-      var object = new THREE.SVGObject( node );
+      var object = new THREE.SVGObject(node);
       scene.add(object);
 
-} );
+    });
 
-render();
-
-/*
-THREE.LegacySVGLoader = function(manager) {
-
-  this.manager = (manager !== undefined) ? manager : THREE.DefaultLoadingManager;
-
-};
-
-THREE.LegacySVGLoader.prototype = {
-
-  constructor: THREE.LegacySVGLoader,
-
-  load: function(url, onLoad, onProgress, onError) {
-
-    var scope = this;
-
-    var parser = new DOMParser();
-
-    var loader = new THREE.FileLoader(scope.manager);
-    loader.load(url, function(svgString) {
-
-      var doc = parser.parseFromString(svgString, 'image/svg+xml'); // application/xml
-
-      onLoad(doc.documentElement);
-
-    }, onProgress, onError);
-
-  }
-
-};
-
-var svgManager = new THREE.LegacySVGLoader();
-var url = 'https://cors-anywhere.herokuapp.com/https://wwwlab.iit.his.se/a17maxsj/map.svg';
-
-function svg_loading_done_callback(doc) {
-  init(new THREE.SVGObject(doc));
   render();
-};
 
-svgManager.load(url,
-  svg_loading_done_callback,
-  function() {
-    console.log("Loading SVG...");
-  },
-  function() {
-    console.log("Error loading SVG!");
-  });
-
-var camera, scene, renderer;
-
-function init(svgObject) {
-  camera = new THREE.PerspectiveCamera(50, 500 / 400, 0.1, 1000);
-  camera.position.z = 100;
-
+  /*
+  THREE.LegacySVGLoader = function(manager) {
   
-  svgObject.position.x = Math.random() * innerWidth;
-  svgObject.position.y = 200;
-  svgObject.position.z = Math.random() * 10000 - 5000;
-  svgObject.scale.x = svgObject.scale.y = svgObject.scale.z = 0.01;
-
-  scene = new THREE.Scene();
-  scene.add(svgObject);
-
+    this.manager = (manager !== undefined) ? manager : THREE.DefaultLoadingManager;
   
-
-  renderer = new THREE.SVGRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight - 5);
-
-document.body.appendChild(renderer.domElement);
-}
-
-
-
-function render() {
-  console.log("Rendering SVG...");
-  renderer.render(scene, camera);
-}
-*/
+  };
+  
+  THREE.LegacySVGLoader.prototype = {
+  
+    constructor: THREE.LegacySVGLoader,
+  
+    load: function(url, onLoad, onProgress, onError) {
+  
+      var scope = this;
+  
+      var parser = new DOMParser();
+  
+      var loader = new THREE.FileLoader(scope.manager);
+      loader.load(url, function(svgString) {
+  
+        var doc = parser.parseFromString(svgString, 'image/svg+xml'); // application/xml
+  
+        onLoad(doc.documentElement);
+  
+      }, onProgress, onError);
+  
+    }
+  
+  };
+  
+  var svgManager = new THREE.LegacySVGLoader();
+  var url = 'https://cors-anywhere.herokuapp.com/https://wwwlab.iit.his.se/a17maxsj/map.svg';
+  
+  function svg_loading_done_callback(doc) {
+    init(new THREE.SVGObject(doc));
+    render();
+  };
+  
+  svgManager.load(url,
+    svg_loading_done_callback,
+    function() {
+      console.log("Loading SVG...");
+    },
+    function() {
+      console.log("Error loading SVG!");
+    });
+  
+  var camera, scene, renderer;
+  
+  function init(svgObject) {
+    camera = new THREE.PerspectiveCamera(50, 500 / 400, 0.1, 1000);
+    camera.position.z = 100;
+  
+    
+    svgObject.position.x = Math.random() * innerWidth;
+    svgObject.position.y = 200;
+    svgObject.position.z = Math.random() * 10000 - 5000;
+    svgObject.scale.x = svgObject.scale.y = svgObject.scale.z = 0.01;
+  
+    scene = new THREE.Scene();
+    scene.add(svgObject);
+  
+    
+  
+    renderer = new THREE.SVGRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight - 5);
+  
+  document.body.appendChild(renderer.domElement);
+  }
+  
+  
+  
+  function render() {
+    console.log("Rendering SVG...");
+    renderer.render(scene, camera);
+  }
+  */
 }
 
 
