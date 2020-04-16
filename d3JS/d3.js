266d5c7279eabcc1e459fb0902505cc59975b379
Max <a17maxsj@student.his.se>
2020-04-16 20:34:08 +0200

    Färgkodning löst, fulade in storlek. ska fixas

--- d3JS/d3.js
+++ d3JS/d3.js
@@ -22,50 +22,76 @@ function d3js() {
 
     var i;
     for (i = 0; i < dList.length; i++) {
-        console.log(i + "medelålder: " + dList[i].dValue)
+        console.log(i + "value: " + dList[i].dValue)
+
+        if (dList[i].dValue >= 1000) {
+            dColor = "#630601";
+            dSize = 25;
+        } else if (dList[i].dValue >= 750) {
+            dColor = "#A7120D";
+            dSize = 20;
+        } else if (dList[i].dValue >= 500) {
+            dColor = "#DE2921";
+            dSize = 15;
+        } else if (dList[i].dValue >= 100) {
+            dColor = "#F26A48";
+            dSize = 10;
+        } else if (dList[i].dValue >= 50) {
+            dColor = "#F59373";
+            dSize = 7;
+        } else if (dList[i].dValue <= 50) {
+            dColor = "#F8BCA2";
+            dSize = 5;
+
+        } else {
+            console.log("color picker error");
+
+        }
+
+
 
         if (dList[i].lan === "01") {
-            drawCircle(460, 430, 5, "Stockholm: " + dList[i].dValue);
+            drawCircle(460, 430, dSize, "Stockholm: " + dList[i].dValue, dColor);
         } else if (dList[i].lan === "03") {
-            drawCircle(450, 410, 5, "Uppsala: " + dList[i].dValue);
+            drawCircle(450, 410, dSize, "Uppsala: " + dList[i].dValue, dColor);
         } else if (dList[i].lan === "04") {
-            drawCircle(435, 445, 5, "Södermanland: " + dList[i].dValue);
+            drawCircle(435, 445, dSize, "Södermanland: " + dList[i].dValue, dColor);
         } else if (dList[i].lan === "05") {
-            drawCircle(420, 470, 5, "Östergötland: " + dList[i].dValue);
+            drawCircle(420, 470, dSize, "Östergötland: " + dList[i].dValue, dColor);
         } else if (dList[i].lan === "06") {
-            drawCircle(400, 500, 5, "Jönköping: " + dList[i].dValue);
+            drawCircle(400, 500, dSize, "Jönköping: " + dList[i].dValue, dColor);
         } else if (dList[i].lan === "07") {
-            drawCircle(405, 525, 5, "Kronoberg: " + dList[i].dValue);
+            drawCircle(405, 525, dSize, "Kronoberg: " + dList[i].dValue, dColor);
         } else if (dList[i].lan === "08") {
-            drawCircle(425, 510, 5, "Kalmar: " + dList[i].dValue);
+            drawCircle(425, 510, dSize, "Kalmar: " + dList[i].dValue, dColor);
         } else if (dList[i].lan === "09") {
-            drawCircle(465, 505, 5, "Gotland: " + dList[i].dValue);
+            drawCircle(465, 505, dSize, "Gotland: " + dList[i].dValue, dColor);
         } else if (dList[i].lan === "10") {
-            drawCircle(418, 542, 5, "Blekinge: " + dList[i].dValue);
+            drawCircle(418, 542, dSize, "Blekinge: " + dList[i].dValue, dColor);
         } else if (dList[i].lan === "12") {
-            drawCircle(390, 550, 5, "Skåne: " + dList[i].dValue);
+            drawCircle(390, 550, dSize, "Skåne: " + dList[i].dValue, dColor);
         } else if (dList[i].lan === "13") {
-            drawCircle(380, 525, 5, "Halland: " + dList[i].dValue);
+            drawCircle(380, 525, dSize, "Halland: " + dList[i].dValue, dColor);
         } else if (dList[i].lan === "14") {
-            drawCircle(380, 475, 5, "Västra götaland: " + dList[i].dValue);
+            drawCircle(380, 475, dSize, "Västra götaland: " + dList[i].dValue, dColor);
         } else if (dList[i].lan === "17") {
-            drawCircle(380, 420, 5, "Värmland: " + dList[i].dValue);
+            drawCircle(380, 420, dSize, "Värmland: " + dList[i].dValue, dColor);
         } else if (dList[i].lan === "18") {
-            drawCircle(410, 430, 5, "Örebro: " + dList[i].dValue);
+            drawCircle(410, 430, dSize, "Örebro: " + dList[i].dValue, dColor);
         } else if (dList[i].lan === "19") {
-            drawCircle(430, 420, 5, "Västmanland: " + dList[i].dValue);
+            drawCircle(430, 420, dSize, "Västmanland: " + dList[i].dValue, dColor);
         } else if (dList[i].lan === "20") {
-            drawCircle(405, 380, 5, "Dalarna: " + dList[i].dValue);
+            drawCircle(405, 380, dSize, "Dalarna: " + dList[i].dValue, dColor);
         } else if (dList[i].lan === "21") {
-            drawCircle(435, 370, 5, "Gävleborg: " + dList[i].dValue);
+            drawCircle(435, 370, dSize, "Gävleborg: " + dList[i].dValue, dColor);
         } else if (dList[i].lan === "22") {
-            drawCircle(449, 300, 5, "Västernorrland: " + dList[i].dValue);
+            drawCircle(449, 300, dSize, "Västernorrland: " + dList[i].dValue, dColor);
         } else if (dList[i].lan === "23") {
-            drawCircle(400, 300, 5, "Jämtland: " + dList[i].dValue);
+            drawCircle(400, 300, dSize, "Jämtland: " + dList[i].dValue, dColor);
         } else if (dList[i].lan === "24") {
-            drawCircle(450, 220, 5, "Västerbotten: " + dList[i].dValue);
+            drawCircle(450, 220, dSize, "Västerbotten: " + dList[i].dValue, dColor);
         } else if (dList[i].lan === "25") {
-            drawCircle(480, 120, 5, "Norrbotten: " + dList[i].dValue);
+            drawCircle(480, 120, dSize, "Norrbotten: " + dList[i].dValue, dColor);
         } else {
             console.log("fail");
             break;
@@ -79,16 +105,17 @@ function d3js() {
     drawCircle(450, 220, 5, dList[2]);
     */
 
-    function drawCircle(x, y, size, tip) {
+    function drawCircle(x, y, size, tip, dotColor) {
 
-        console.log('Drawing circle at', x, y, size);
+        console.log('Drawing circle at', x, y, size, dotColor);
         sampleSVG.append("circle")
 
         .style("stroke", "gray")
-            .style("fill", "blue")
+            .style("fill", dotColor)
             .attr("cx", x)
             .attr("cy", y)
             .attr("r", size)
+            .style("opacity", 0.7)
             .on("mouseover", function() { return tooltip.style("visibility", "visible"); })
             .on("mousemove", function() { return tooltip.style("top", (event.pageY - 10) + "px").style("left", (event.pageX + 10) + "px"); })
             .on("mouseout", function() { return tooltip.style("visibility", "hidden"); });
